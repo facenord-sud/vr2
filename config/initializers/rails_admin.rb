8013a198e992f2f40b9d5548f0e18e13b80f4ca3
leo <facenord.sud@gmail.com>
2014-02-19 15:23:03 +0100

    correction du bug 3 de portrait

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -22,62 +22,6 @@ RailsAdmin.config do |config|
 
    config.excluded_models << 'Contact'
 
-  #  ==> Global show view settings
-  # Display empty fields in show views
-  # config.compact_show_view = false
-
-  #  ==> Global list view settings
-  # Number of default rows per-page:
-  # config.default_items_per_page = 20
-
-  #  ==> Included models
-  # Add all excluded models here:
-  # config.excluded_models = [User]
-
-  # Add models here if you want to go 'whitelist mode':
-  # config.included_models = [User]
-
-  # Application wide tried label methods for models' instances
-  # config.label_methods << :description # Default is [:name, :title]
-
-  #  ==> Global models configuration
-  # config.models do
-  #   # Configuration here will affect all included models in all scopes, handle with care!
-  #
-  #   list do
-  #     # Configuration here will affect all included models in list sections (same for show, export, edit, update, create)
-  #
-  #     fields_of_type :date do
-  #       # Configuration here will affect all date fields, in the list section, for all included models. See README for a comprehensive type list.
-  #     end
-  #   end
-  # end
-  #
-  #  ==> Model specific configuration
-  # Keep in mind that *all* configuration blocks are optional.
-  # RailsAdmin will try his best to provide the best defaults for each section, for each field.
-  # Try to override as few things as possible, in the most generic way. Try to avoid setting labels for models and attributes, use ActiveRecord I18n API instead.
-  # Less code is better code!
-  # config.model MyModel do
-  #   # Cross-section field configuration
-  #   object_label_method :name     # Name of the method called for pretty printing an *instance* of ModelName
-  #   label 'My model'              # Name of ModelName (smartly defaults to ActiveRecord's I18n API)
-  #   label_plural 'My models'      # Same, plural
-  #   weight -1                     # Navigation priority. Bigger is higher.
-  #   parent OtherModel             # Set parent model for navigation. MyModel will be nested below. OtherModel will be on first position of the dropdown
-  #   navigation_label              # Sets dropdown entry's name in navigation. Only for parents!
-  #   # Section specific configuration:
-  #   list do
-  #     filters [:id, :name]  # Array of field names which filters should be shown by default in the table header
-  #     items_per_page 100    # Override default_items_per_page
-  #     sort_by :id           # Sort column (default is primary key)
-  #     sort_reverse true     # Sort direction (default is true for primary key, last created first)
-  #     # Here goes the fields configuration for the list view
-  #   end
-  # end
-
-
-
    config.model Post do
      edit do
        field :title
@@ -125,10 +69,6 @@ RailsAdmin.config do |config|
   end
 
   config.model Image do
-
-    object_label_method do
-      :image_label_method
-    end
     edit do
       field :title
       field :asset
