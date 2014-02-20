cffbb622a414db0dbe5fc93eaf21da617b26bbca
leo <facenord.sud@gmail.com>
2014-02-20 16:17:06 +0100

    corriger contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,8 +5,8 @@ class ContactsController < ApplicationController
     if @contact.valid?
       ActionMailer::Base.mail(
           from: @contact.email, :to => 'val.rota@hotmail.com',
-          subject: @contact.title,
-          body: @contact.body
+          subject: "#{@contact.email}  t'as envoyé un message depuis valentin-rota.ch",
+          body: "#{@contact.email} a envoyé le message suivant:\n Titre : #{@contact.title}\nMessage : #{@contact.body}"
       ).deliver
       redirect_to contact_path, flash: {success: "Votre e-mail (#{@contact.title}) a été envoyé avec succès.
 Je vous répondrai au plus tôt à l'adresse: #{@contact.email}"}
