0b77a0b70850841c3cb44e703db0ad6b12d4f555
leo <facenord.sud@gmail.com>
2014-04-01 13:19:22 +0200

    fait tous les todos excepté les images

--- app/controllers/news_letters_controller.rb
+++ app/controllers/news_letters_controller.rb
@@ -2,7 +2,7 @@ class NewsLettersController < ApplicationController
   def create
     @news_letter = NewsLetter.new email: params[:news_letter][:email]
     if @news_letter.save
-      redirect_to root_path, notice: 'Vous avez été abboné à la newsletter'
+      redirect_to root_path, notice: 'Vous avez été abonné à la newsletter'
     else
       @posts = Post.published.order('updated_at DESC').includes(:test_image).page(params[:page]).per(15)
       @event = Event.next
--- config.ru
+++ config.ru
@@ -1,5 +1,5 @@
 # This file is used by Rack-based servers to start the application.
 
 require ::File.expand_path('../config/environment',  __FILE__)
-use Rack::CanonicalHost, 'www.valentin-rota.ch'
+use Rack::CanonicalHost, 'www.valentin-rota.ch' if Rails.env == 'production'
 run Collaide::Application
